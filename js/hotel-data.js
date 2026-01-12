961c6c65afbcd26290835fe7b1e0ba94cf778118
fwisheu <F.Wisheu@lmu.de>
2026-01-12 11:23:41 +0100

    Create hotel-data.js

---
+++ js/hotel-data.js
@@ -0,0 +1,18 @@
+// Zentrales Hotel-Datenobjekt
+const HOTEL_DATA = [
+  {
+    id: "h1",
+    name: "Hotel Alpha",
+    basePrice: 100
+  },
+  {
+    id: "h2",
+    name: "Hotel Beta",
+    basePrice: 90
+  },
+  {
+    id: "h3",
+    name: "Hotel Gamma",
+    basePrice: 110
+  }
+];
